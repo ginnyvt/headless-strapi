f6784a177ff6d6f9398fb366f83c08a9ebb303fe
ginnyvt <ginny@powerkernel.com>
2024-08-04 16:00:19 +0300

    Setup strapi

---
+++ README.md
@@ -0,0 +1,7 @@
+# Headless
+
+Headless CMS for my official website.
+
+## Actions permissions
+
+Go to Settings -> Actions > General > Workflow permissions and select `Read and write permissions`
---
+++ types/generated/components.d.ts
@@ -0,0 +1,90 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SharedSocialLink extends Schema.Component {
+  collectionName: 'components_shared_social_links';
+  info: {
+    displayName: 'socialLink';
+    icon: 'earth';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    href: Attribute.String & Attribute.Required;
+    icon: Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
+      Attribute.Required;
+  };
+}
+
+export interface SharedSeo extends Schema.Component {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'search';
+  };
+  attributes: {
+    metaTitle: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    metaDescription: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 50;
+        maxLength: 160;
+      }>;
+    metaImage: Attribute.Media<'images' | 'files' | 'videos'>;
+    metaSocial: Attribute.Component<'shared.meta-social', true>;
+    keywords: Attribute.Text;
+    metaRobots: Attribute.String;
+    structuredData: Attribute.JSON;
+    metaViewport: Attribute.String;
+    canonicalURL: Attribute.String;
+  };
+}
+
+export interface SharedMetaSocial extends Schema.Component {
+  collectionName: 'components_shared_meta_socials';
+  info: {
+    displayName: 'metaSocial';
+    icon: 'project-diagram';
+  };
+  attributes: {
+    socialNetwork: Attribute.Enumeration<['Facebook', 'Twitter']> &
+      Attribute.Required;
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+    description: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        maxLength: 65;
+      }>;
+    image: Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedLink extends Schema.Component {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'link';
+    icon: 'link';
+  };
+  attributes: {
+    href: Attribute.String & Attribute.Required;
+    label: Attribute.String & Attribute.Required;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'shared.social-link': SharedSocialLink;
+      'shared.seo': SharedSeo;
+      'shared.meta-social': SharedMetaSocial;
+      'shared.link': SharedLink;
+    }
+  }
+}
